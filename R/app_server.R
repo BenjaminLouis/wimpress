fc6399b1188274c5816dcd5c6db333a73a934bbd
Benjamin Louis <benjaminlouis.pro@gmail.com>
2018-11-27 00:39:46 +0100

    :up::construction: Update(all) :
    change functions to be "pages oriented"
    add writing og margin properties scss file
    sass compilation does't wrok because either @import is control directives or @extend is not in a classic css rule (but in a css for pages media rule)

--- R/app_server.R
+++ R/app_server.R
@@ -2,54 +2,57 @@
 #'
 app_server <- function(input, output, session) {
 
-  page <- callModule(mod_cssproperty_page, "page")
-  tlc <- callModule(mod_cssproperty_margin, "top-left-corner")
-  tl <- callModule(mod_cssproperty_margin, "top-left")
-  tc <- callModule(mod_cssproperty_margin, "top-center")
-  tr <- callModule(mod_cssproperty_margin, "top-right")
-  trc <- callModule(mod_cssproperty_margin, "top-right-corner")
-  rt <- callModule(mod_cssproperty_margin, "right-top")
-  rm <- callModule(mod_cssproperty_margin, "right-middle")
-  rb <- callModule(mod_cssproperty_margin, "right-bottom")
-  brc <- callModule(mod_cssproperty_margin, "bottom-right-corner")
-  br <- callModule(mod_cssproperty_margin, "bottom-right")
-  bc <- callModule(mod_cssproperty_margin, "bottom-center")
-  bl <- callModule(mod_cssproperty_margin, "bottom-left")
-  blc <- callModule(mod_cssproperty_margin, "bottom-left-corner")
-  lb <- callModule(mod_cssproperty_margin, "left-bottom")
-  lm <- callModule(mod_cssproperty_margin, "left-middle")
-  lt <- callModule(mod_cssproperty_margin, "left-top")
+  all <- callModule(mod_cssproperty_page, "all")
+  first <- callModule(mod_cssproperty_page, "first")
+  last <- callModule(mod_cssproperty_page, "last")
+  left <- callModule(mod_cssproperty_page, "left")
+  right <- callModule(mod_cssproperty_page, "right")
 
-  wholelist <- reactive(list("page" = page, "top-left-corner" = tlc, "top-left" = tl,
-    "top-center" = tc, "top-right" = tr, "top-right-corner" = trc, "right-top" = rt,
-    "right-middle" = rm, "right-bottom" = rb, "bottom-right-corner" = brc, "bottom-right" = br,
-    "bottom-center" = bc, "bottom-left" = bl, "bottom-left-corner" = blc, "left-bottom" = lb,
-    "left-middle" = lm, "left-top" = lt))
+  wholelist <- reactive(list("all" = all, "first" = first, "last" = last,
+                             "left" = left, "right" = right))
+
+
   boolevar <- reactive({
-    paste0(
-      "$boolall: ", any(sapply(wholelist(), getbool, what = "All")), ";\n",
-      "$boolfirst: ", any(sapply(wholelist(), getbool, what = "First")), ";\n",
-      "$boollast: ", any(sapply(wholelist(), getbool, what = "Last")), ";\n",
-      "$boolleft: ", any(sapply(wholelist(), getbool, what = "Left")), ";\n",
-      "$boolright: ", any(sapply(wholelist(), getbool, what = "Right")), ";\n"
-    )
+    ll <- lapply(1:length(wholelist()), function(i) {
+        getbool(wholelist()[[i]], names(wholelist())[i])
+    })
+    paste0(ll, collapse = "")
   })
 
-  genprop <- reactive(getprop(page, wh = "prop"))
 
-  # allmarginlist <- reactive({
-  #   res <- lapply(1:(length(wholelist()) - 1), function(i) getlist(wholelist()[[i + 1]], marginname = names(wholelist())[i + 1]))
-  #   pagetype <- unlist(sapply(res, names))
-  #   pagetype <- unique(pagetype[!is.na(pagetype)])
-  #
-  # })
-  #
-  # observe(print(allmarginlist()))
+  genprop <- reactive({
+    ll <- lapply(1:length(wholelist()), function(i) {
+      getprop(wholelist()[[i]], names(wholelist())[i])
+    })
+    paste0(ll[ll != ""], collapse = "\n")
+  })
 
 
-  observeEvent(input$refresh, {
-    write(x = boolevar(), file = normalizePath(file.path(system.file("www", package = "wimpress"), "_boolevar.scss"), mustWork = FALSE, winslash = "/"))
-    write(x = genprop(), file = normalizePath(file.path(system.file("www", package = "wimpress"), "_genprop.scss"), mustWork = FALSE, winslash = "/"))
-    callModule(mod_view_renderedpdf, "my_pdf", path = system.file("www", package = "wimpress"))
-  }, ignoreNULL = FALSE)
+
+  allmarginlist <- reactive({
+    lapply(wholelist(), getlist)
+    # pagetype <- unlist(sapply(res, names))
+    # pagetype <- unique(pagetype[!is.na(pagetype)])
+
+  })
+
+
+  # # Only x=when the button is
+  # observeEvent(input$refresh, {
+  #   # Writing boolean variables
+  #   write(x = boolevar(), file = normalizePath(file.path(system.file("www", package = "wimpress"), "_boolevar.scss"), mustWork = FALSE, winslash = "/"))
+  #   # Writing pages general css property
+  #   write(x = genprop(), file = normalizePath(file.path(system.file("www", package = "wimpress"), "_genprop.scss"), mustWork = FALSE, winslash = "/"))
+  #   #writing margins css property
+  #   lapply(1:length(allmarginlist()), function(i) {
+  #     write(x = allmarginlist()[[i]], file = normalizePath(file.path(system.file("www", package = "wimpress"), paste0("_margin", names(allmarginlist())[i], ".scss")), mustWork = FALSE, winslash = "/"))
+  #   })
+  #   #Compiling SCSS to CSS
+  #   sass::sass(input = sass::sass_import(normalizePath(file.path(system.file("www", package = "wimpress"), "template_style.scss"))),
+  #              output = normalizePath(file.path(system.file("www", package = "wimpress"), "style.css")))
+  #   #rendered the pdf
+  #   callModule(mod_view_renderedpdf, "my_pdf", path = system.file("www", package = "wimpress"),
+  #              style_css = normalizePath(file.path(system.file("www", package = "wimpress"), "style.css")))
+  # }, ignoreNULL = FALSE)
+
 }
--- R/internal_function.R
+++ R/internal_function.R
@@ -1,55 +1,74 @@
 getbool <- function(x, what) {
-  if (is.null(x)) {
-    FALSE
+  if (length(x$prop()) == 0) {
+    res <- FALSE
   } else {
-    any(x$where() == what)
+    res <- any(x$prop() != "" & x$value() != "")
   }
+  paste0("$bool", what,": ", as.character(res), ";\n")
 }
 
 
 getprop <- function(x, wh) {
-  ll <- lapply(unique(x$where()), function(y) paste0(x$prop()[x$where() == y], ": ", x$value()[x$where() == y]))
-  ll <- lapply(ll, function(y) gsub("(^:.*|.*:\\s$)", "", y))
-  ll <- lapply(ll, function(y) paste(y, collapse = ", "))
-  ll <- lapply(ll, function(y) gsub(",,", ",", y))
-  ll <- lapply(ll, function(y) paste0("(", y, ")"))
-  ll <- paste0("$", wh, tolower(unique(x$where())), ": ", ll)
-  ll <- lapply(ll, function(y) {
-    if (grepl("^.*:\\s\\(?(,?\\s?)*\\)?$", y)) {
-      NULL
-    } else {
-      paste0(y, ";")
-    }
-  })
-  ll <- lapply(ll, function(y) gsub(", \\)", "\\)", y))
-  ll <- lapply(ll, function(y) gsub("\\(,\\s", "\\(", y))
-  ll <- paste(unlist(ll), collapse = "\n")
+  if (length(x$prop()) == 0) {
+    ll <- ""
+  } else {
+    ll <- paste0(x$prop()[x$where() == "none"], ": ", x$value()[x$where() == "none"])
+    ll <- gsub("(^:.*|.*:\\s$)", "", ll)
+    ll <- paste(ll, collapse = ", ")
+    ll <- gsub(",,", ",", ll)
+    ll <- paste0("(", ll, ")")
+    ll <- paste0("$prop", wh, ": ", ll)
+    ll <- if (grepl("^.*:\\s\\(?(,?\\s?)*\\)?$", ll)) {
+         ""
+       } else {
+         paste0(ll, ";")
+       }
+    ll <- gsub(", \\)", "\\)", ll)
+    ll <- gsub("\\(,\\s", "\\(", ll)
+  }
   return(ll)
 }
 
 
-getlist <- function(x, marginname) {
-  ll <- lapply(unique(x$where()), function(y) paste0(x$prop()[x$where() == y], ": ", x$value()[x$where() == y]))
-  ll <- lapply(ll, function(y) gsub("(^:.*|.*:\\s$)", "", y))
-  ll <- lapply(ll, function(y) paste(y, collapse = ", "))
-  ll <- lapply(ll, function(y) gsub(",,", ",", y))
-  ll <- lapply(ll, function(y) paste0("(", y, ")"))
-  ll <- paste0("$prop: ", ll)
-  ll <- lapply(ll, function(y) {
-    if (grepl("^.*:\\s\\(?(,?\\s?)*\\)?$", y)) {
-      NULL
-    } else {
-      paste0(y, ";")
+getlist <- function(x) {
+  if (length(x$prop()) == 0 | length(setdiff(unique(x$where()), "none")) == 0) {
+    ll <- ""
+  } else {
+    ll <- lapply(setdiff(unique(x$where()), "none"), function(y) {
+      paste0(x$prop()[x$where() == y], ": ", x$value()[x$where() == y])
+    })
+    ll <- lapply(ll, function(y) gsub("(^:.*|.*:\\s$)", "", y))
+    ll <- lapply(ll, function(y) paste(y, collapse = ", "))
+    ll <- lapply(ll, function(y) gsub(",,", ",", y))
+    ll <- lapply(ll, function(y) paste0("(", y, ")"))
+    ll <- paste0("$prop: ", ll)
+    ll <- lapply(ll, function(y) {
+      if (grepl("^.*:\\s\\(?(,?\\s?)*\\)?$", y)) {
+        ""
+      } else {
+        paste0(y, ";")
+      }
+    })
+    newnames <- strsplit(setdiff(unique(x$where()), "none"), "-")
+    newnames <- lapply(newnames, gsub, pattern = "^t$", replacement = "top")
+    newnames <- lapply(newnames, gsub, pattern = "^b$", replacement = "bottom")
+    newnames <- lapply(newnames, gsub, pattern = "^l$", replacement = "left")
+    newnames <- lapply(newnames, gsub, pattern = "^r$", replacement = "right")
+    newnames <- lapply(newnames, gsub, pattern = "^m$", replacement = "middle")
+    newnames <- lapply(newnames, gsub, pattern = "^ce$", replacement = "center")
+    newnames <- lapply(newnames, gsub, pattern = "^co$", replacement = "corner")
+    newnames <- sapply(newnames, paste0, collapse = "-")
+    ll <- lapply(1:length(ll), function(i) {
+      if (ll[[i]] == "") {
+        ll[[i]]
+      } else {
+        sass::sass(list(ll[[i]], paste0("@", newnames[i], " { @each $var, $val in $prop { #{$var}: $val; } }")))
+      }
+    })
+    if (any(sapply(ll, function(y) y != ""))) {
+      ll <- sass::sass(as.list(ll))
     }
-  })
-  names(ll) <- tolower(unique(x$where()))
-  ll <- lapply(ll, function(y) {
-    if (is.null(y)) {
-      y
-    } else {
-      style <- sass::sass(list(y, paste0("@", marginname, " { @each $var, $val in $prop { #{$var}: $val; } }")))
-    }
-  })
+  }
   return(ll)
 }
 
