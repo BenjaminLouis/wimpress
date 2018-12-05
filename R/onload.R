c838805f2fb63d27771b9c35d7fca1cac1280835
Benjamin Louis <benjaminlouis.pro@gmail.com>
2018-12-05 20:24:31 +0100

    add onload file

---
+++ R/onload.R
@@ -0,0 +1,9 @@
+
+#' Adds the content of www to www/ from this package
+#'
+#' @importFrom shiny addResourcePath
+#'
+#' @noRd
+.onLoad <- function(...) {
+  shiny::addResourcePath('www', system.file('www', package = 'wimpress'))
+}
